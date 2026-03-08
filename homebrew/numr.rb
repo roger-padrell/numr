0c08dbac4a9c16189c40720de5f9f15163e396b0
Ben Nasedkin <you@example.com>
2026-03-08 16:40:35 +0200

    chore: bump version to v0.5.2

--- homebrew/numr.rb
+++ homebrew/numr.rb
@@ -1,7 +1,7 @@
 class Numr < Formula
   desc "Text calculator for natural language expressions with a vim-style TUI"
   homepage "https://github.com/nasedkinpv/numr"
-  version "0.5.1"
+  version "0.5.2"
   license "MIT"
 
   on_macos do
